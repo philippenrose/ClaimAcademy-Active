04bcf75aac9c3730fdd140e6dba9b911c99682b0
philippenrose <46907267+philippenrose@users.noreply.github.com>
2019-02-26 18:55:03 -0600

    Add files via upload

---
+++ src/components/to-do/TodoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const todoList = (props) => {
+    return props.items.map((item, index)=>(
+    <li key={index}>
+            {item}
+     </li>
+))
+}
+export default todoList;
